b81d5077de00a8e3ff668cdb65e2106d463f34cd
Valeriy Kunitskiy <116190795+vvkunitskiy@users.noreply.github.com>
2023-01-20 19:23:08 +0300

    script for stage 1
    Приложение для распознавания текстовых блоков на чеке. Выдаёт координаты найденных блоков и текст в них

---
+++ easyocr_run.py
@@ -0,0 +1,89 @@
+import easyocr
+
+import os
+import os.path
+from pathlib import Path
+
+input_directory = input('Input directory is:\n')
+if not os.path.exists(input_directory):
+	print('Wrong path!')
+	exit()
+if input_directory != '' and input_directory[-1] != '/':
+	input_directory += '/'
+ 
+output_directory = input('Output directory is:\n')
+if not os.path.exists(output_directory):
+	print('Wrong path!')
+	exit()
+if output_directory != '' and output_directory[-1] != '/':
+	output_directory += '/'
+	
+#print(easyocr.__all__)
+
+for root, dirs, files in os.walk(input_directory):  
+	for filename in files: 
+		
+		file_path = root+filename
+		record_to_file = ''
+		
+		reader = easyocr.Reader(["en"])
+		try:
+			image_predict = reader.readtext(file_path)
+		except:
+			print(file_path,'is WRONG file!')
+			continue
+			
+		
+		try:
+			mediafile_extension = Path(file_path).suffixes[-1]
+		except:
+			mediafile_extension = ''
+			
+		file_path = file_path.removesuffix(mediafile_extension)
+			
+		for line in image_predict:
+			coordinates = str(line[0])
+			text_box = line[1]
+
+			coordinates = coordinates.replace('[','')
+			coordinates = coordinates.replace(' ','')
+			coordinates = coordinates.replace(']','')
+
+			record_to_file += coordinates + ',' + text_box + '\n'
+		
+		recording_file = open(file_path+'-1.txt', 'w+')
+		recording_file.write(record_to_file)
+		
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
